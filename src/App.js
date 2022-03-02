22d9737d1bda5a39b468324910c6d43288ea3795
ambiti0n <fabulousfresh010@gmail.com>
2022-03-02 02:05:09 +0100

    homepage textAnimation BackgroundImage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+      <title>Travel stories | Exciting tours for adventurous people</title>
+
+      <header className="header">
+            <div className="header__logo-box">
+                <img src="img/logo-white.png" alt="Logo" className="header__logo" />
+            </div>
+
+            <div className="header__text-box">
+                <h1 className="heading-primary">
+                    <span className="heading-primary--main">Travelogs</span>
+                    <span className="heading-primary--sub">Document your Memories</span>
+                </h1>
+
+                <a href="#section-tours" className="btn btn--white btn--animated">Post Your Travel Stories</a>
+            </div>
+        </header>
+
+   </div>
   );
 }
 
