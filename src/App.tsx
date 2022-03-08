d45aecee3043f605a88d0e5896d4cecd802127e8
ambiti0n <fabulousfresh010@gmail.com>
2022-03-08 09:51:34 +0100

    footer_implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import Homepage from "./pages/Homepage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <Homepage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
